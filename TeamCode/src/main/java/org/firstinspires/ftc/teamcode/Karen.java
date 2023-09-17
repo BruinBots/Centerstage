9f97cd2513429370d8573fb92ab61be7e9ee72c5
Sandra Mattar <69043333+smattar2020@users.noreply.github.com>
2023-09-17 16:39:38 -0400

    add omniwheel support and fix bugs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Karen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Karen.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
-import com.qualcomm.robotcore.util.ElapsedTime;
 
-public class Karen  {
+public class Karen {
+
+
+    // Class variables
 
     public DcMotorEx leftFrontMotor;
     public DcMotorEx rightFrontMotor;
@@ -12,26 +14,31 @@ public class Karen  {
     public DcMotorEx rightBackMotor;
 
     // constructor with map
-    public Karen (HardwareMap map) {
+    public Karen(HardwareMap map) {
         // Drivetrain Motors
-        leftFrontMotor = map.get(DcMotorEx.class, "leftFrontMotor");
-        rightFrontMotor = map.get(DcMotorEx.class, "rightFrontMotor");
-        leftBackMotor = map.get(DcMotorEx.class, "leftBackMotor");
-        rightBackMotor = map.get(DcMotorEx.class, "rightBackMotor");
+        leftFrontMotor = map.get(DcMotorEx.class, "left_omni");
+        rightFrontMotor = map.get(DcMotorEx.class, "right_omni");
+        leftBackMotor = map.get(DcMotorEx.class, "left_drive");
+        rightBackMotor = map.get(DcMotorEx.class, "right_drive");
 
-        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
+//        leftBackMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+//        leftFrontMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
         leftFrontMotor.setDirection(DcMotorEx.Direction.REVERSE);
+        leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
+
     }
 
-    public void moveBot(double drive, double rotate, double strafe, double scaleFactor) {
+    public void moveBot(double drive, double rotate, double scaleFactor) {
         double[] wheelSpeeds = new double[4];
-        wheelSpeeds[0] = drive + strafe + rotate;  // left front
-        wheelSpeeds[1] = drive - strafe - rotate;  // right front
-        wheelSpeeds[2] = drive - strafe + rotate;  // left rear
-        wheelSpeeds[3] = drive + strafe - rotate;  // right rear
+        wheelSpeeds[0] = drive + rotate;  // left front
+        wheelSpeeds[1] = drive - rotate;  // right front
+        wheelSpeeds[2] = drive + rotate;  // left rear
+        wheelSpeeds[3] = drive - rotate;  // right rear
 
         // finding the greatest power value
         double maxMagnitude = Math.max(Math.max(Math.max(wheelSpeeds[0], wheelSpeeds[1]), wheelSpeeds[2]), wheelSpeeds[3]);
+
 
         // dividing everyone by the max power value so that ratios are same (check if sdk automatically clips to see if go build documentation works
         if (maxMagnitude > 1.0)
@@ -42,19 +49,15 @@ public class Karen  {
             }
         }
 
+//        telemetry.addData("power", wheelSpeeds);
+
+
+
         // setting motor power and scaling down to preference
         leftFrontMotor.setPower(wheelSpeeds[0] * scaleFactor);
         rightFrontMotor.setPower(wheelSpeeds[1] * scaleFactor);
         leftBackMotor.setPower(wheelSpeeds[2] * scaleFactor);
         rightBackMotor.setPower(wheelSpeeds[3] * scaleFactor);
-
-    }
-
-    // movebot command that runs for a certain amount of time
-    public void moveBotTimer(double drive, double rotate, double strafe, int time, ElapsedTime runTime){
-        while(((int)(runTime.time() * 1000)) < time){
-            this.moveBot(drive, rotate, strafe, 1);
-        }
     }
 
     public void stop(){
