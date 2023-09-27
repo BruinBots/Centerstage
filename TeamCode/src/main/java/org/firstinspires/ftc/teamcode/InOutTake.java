3b6a591f7a1e11da2addf7ebf440418ea1489b43
Sandra Mattar <69043333+smattar2020@users.noreply.github.com>
2023-09-26 20:19:57 -0400

    remove opmode from inouttake

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InOutTake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InOutTake.java
@@ -1,71 +1,17 @@
 package org.firstinspires.ftc.teamcode;
 
-import static java.lang.Thread.sleep;
-
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 
-@TeleOp(name="Testing: In-outtake TeleOp", group="Iterative Opmode")
-public class InOutTake extends OpMode {
+public class InOutTake {
 
     DcMotorEx motor;
     DigitalChannel sensor;
 
-    Karen bot;
-
-    boolean testing = false;
-
-    @Override
-    public void init() {
-        bot = new Karen(hardwareMap);
-
-        motor = bot.intakeMotor;
-        sensor = bot.intakeTouchSensor;
-
-        telemetry.addData("Status", "Initialized");
-    }
-
-    @Override
-    public void init_loop() {
-    }
-
-    //
-    @Override
-    public void start() {
-    }
-
-    //
-    @Override
-    public void loop() {
-        boolean value = touch();
-
-        if (gamepad1.a) {
-            testing = true;
-        }
-        else if (gamepad1.b) {
-            testing = false;
-        }
-
-        if (value) {
-            testing = false;
-        }
-
-        if (testing) {
-            spin(0.5, DcMotorSimple.Direction.FORWARD);
-        }
-        else {
-            stopSpin();
-        }
-        telemetry.addData("sensor", value);
-
-        try {
-            sleep(20);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void init(DcMotorEx motor, DigitalChannel sensor) {
+        this.motor = motor;
+        this.sensor = sensor;
     }
 
     public void spin(double speed, DcMotorSimple.Direction direction) {
@@ -79,9 +25,5 @@ public class InOutTake extends OpMode {
 
     public boolean touch() {
         return sensor.getState();
-    }
-
-    @Override
-    public void stop() {
     }
 }
