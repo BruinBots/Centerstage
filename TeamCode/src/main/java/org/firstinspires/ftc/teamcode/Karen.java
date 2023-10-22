97a5682d19aa4f3e014f1f6890d812a438c7f601
Win10MC <easpecht09@gmail.com>
2023-10-22 14:30:35 -0400

    remove redundant code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Karen.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Karen.java
@@ -29,37 +29,6 @@ public class Karen {
         leftBackMotor.setDirection(DcMotorEx.Direction.REVERSE);
     }
 
-    public void moveBot(double drive, double rotate, double scaleFactor) {
-        double[] wheelSpeeds = new double[4];
-        wheelSpeeds[0] = drive + rotate;  // left front
-        wheelSpeeds[1] = drive - rotate;  // right front
-        wheelSpeeds[2] = drive + rotate;  // left rear
-        wheelSpeeds[3] = drive - rotate;  // right rear
-
-        // finding the greatest power value
-        double maxMagnitude = Math.max(Math.max(Math.max(wheelSpeeds[0], wheelSpeeds[1]), wheelSpeeds[2]), wheelSpeeds[3]);
-
-
-        // dividing everyone by the max power value so that ratios are same (check if sdk automatically clips to see if go build documentation works
-        if (maxMagnitude > 1.0)
-        {
-            for (int i = 0; i < wheelSpeeds.length; i++)
-            {
-                wheelSpeeds[i] /= maxMagnitude;
-            }
-        }
-
-//        telemetry.addData("power", wheelSpeeds);
-
-
-
-        // setting motor power and scaling down to preference
-        leftFrontMotor.setPower(wheelSpeeds[0] * scaleFactor);
-        rightFrontMotor.setPower(wheelSpeeds[1] * scaleFactor);
-        leftBackMotor.setPower(wheelSpeeds[2] * scaleFactor);
-        rightBackMotor.setPower(wheelSpeeds[3] * scaleFactor);
-    }
-
     public void moveBotMecanum(double drive, double rotate, double strafe, double scaleFactor) {
         double[] wheelSpeeds = new double[4];
         wheelSpeeds[0] = drive + strafe + rotate;  // left front
