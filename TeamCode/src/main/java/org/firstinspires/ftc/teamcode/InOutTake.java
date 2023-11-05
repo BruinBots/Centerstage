e94a25fbaf983186ca8b38a9e0f09363ecdafc9e
3dcoded <maser_assets_02@icloud.com>
2023-11-05 16:24:18 -0500

    clean up code as per second code review

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InOutTake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/InOutTake.java
@@ -4,24 +4,31 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class InOutTake {
 
+    // inServo's are continous rotation servos, rotate backwards of each other to in/out-take the pixel
     private Servo inServoLeft;
     private Servo inServoRight;
-    // above servos are continous rotation servos, rotate backwards of each other to in/out-take the pixel
-    private Servo scoopServo;
-    // scoop servo is standard servo to move pixel between intake and claw
 
-    public static final double IN_SPEED = 0.5; // untested, should work
+    // scoop servo is standard servo to move pixel between intake and claw
+    private Servo scoopServo;
+
+    // untested, should work
+    // SPEED constants are between 0-0.5
+    // 0            0.5
+    // stop         full speed
+    public static final double IN_SPEED = 0.5;
     public static final double OUT_SPEED = 0.3;
 
-    // above constants are between 0-1
+    // STOP_POS constant is between 0-1
     // 0            0.5         1
     // backwards    stop        forwards
+    // 0.5 is (should be) stop
+    public static final double STOP_POS = 0.5;
 
-    public static final double SCOOP_DOWN_POS = 0; // this should work, untested
-    public static final double SCOOP_UP_POS = 0.7; // no idea if this works
-    // above constants are between 0-1
-    // 0        ?       1
+    // SCOOP_POS constants are between 0-1
+    // 0        TBD     1
     // down     up      too far up
+    public static final double SCOOP_DOWN_POS = 0; // this should work, untested
+    public static final double SCOOP_UP_POS = 0.7; // no idea if this works, untested
 
     InOutTake (Servo inServoLeft, Servo inServoRight, Servo scoopServo) {
         this.inServoLeft = inServoLeft;
@@ -29,26 +36,31 @@ public class InOutTake {
         this.scoopServo = scoopServo;
     }
 
-    public void intake() { // suck pixels in
+    // suck pixels in
+    public void intake() {
         inServoLeft.setPosition(0.5 + IN_SPEED);
         inServoRight.setPosition(0.5 - IN_SPEED);
     }
 
-    public void outtake() { // eject pixels out
+    // eject pixels out
+    public void outtake() {
         inServoLeft.setPosition(0.5 + OUT_SPEED);
         inServoRight.setPosition(0.5 - OUT_SPEED);
     }
 
-    public void stopTake() { // stop inServo's
-        inServoLeft.setPosition(0.5);
-        inServoRight.setPosition(0.5);
+    // stop inServo's
+    public void stopTake() {
+        inServoLeft.setPosition(STOP_POS);
+        inServoRight.setPosition(STOP_POS);
     }
 
-    public void scoopUp() { // move pixel form intake --> claw
+    // move pixel from intake --> claw
+    public void scoopUp() {
         scoopServo.setPosition(SCOOP_UP_POS);
     }
 
-    public void scoopDown() { // move intake down to collect next pixel
+    // move intake down to collect next pixel
+    public void scoopDown() {
         scoopServo.setPosition(SCOOP_DOWN_POS);
     }
 }
