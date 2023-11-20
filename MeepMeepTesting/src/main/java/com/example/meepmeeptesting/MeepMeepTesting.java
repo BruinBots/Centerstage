58149f12f3b0a4b44006980cc46038ddce12468a
3dcoded <maser_assets_02@icloud.com>
2023-11-20 17:31:38 -0500

    navigate to left and right spike mark

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -14,15 +14,19 @@ public class MeepMeepTesting {
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(new Pose2d(12, -65, Math.toRadians(90)))
-                                .splineTo(new Vector2d(12, -60), Math.toRadians(90))
-                                .splineTo(new Vector2d(48, -36), Math.toRadians(0))
+                        drive.trajectorySequenceBuilder(new Pose2d(-36, 65, Math.toRadians(90)))
+//                                .splineToConstantHeading(new Vector2d(-26, 32), Math.toRadians(90))
+                                .splineTo(new Vector2d(-36, 60), Math.toRadians(0))
+                                .splineTo(new Vector2d(18, 60), Math.toRadians(0))
+                                .splineTo(new Vector2d(48, 36), Math.toRadians(0))
+                                .splineTo(new Vector2d(40, 36), Math.toRadians(0))
+                                .splineTo(new Vector2d(60, 60), Math.toRadians(0))
                                 .build()
                 );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_CENTERSTAGE_JUICE_DARK)
                 .setDarkMode(true)
-                .setBackgroundAlpha(0.95f)
+                .setBackgroundAlpha(0.99f)
                 .addEntity(myBot)
                 .start();
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueFarBackdrop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/BlueFarBackdrop.java
@@ -6,16 +6,20 @@ import com.acmerobotics.roadrunner.trajectory.Trajectory;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.Arm;
+import org.firstinspires.ftc.teamcode.Karen;
 import org.firstinspires.ftc.teamcode.drive.SampleMecanumDrive;
 
 @Autonomous(name = "BlueFarBackdrop", group = "Autonomous: Testing")
 public class BlueFarBackdrop extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
+        Karen bot = new Karen(hardwareMap);
+
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
 
         // We want to start the bot at x: -36, y: 65, heading: 270 degrees
-        Pose2d startPose = new Pose2d(-36,-65, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(-36,65, Math.toRadians(90));
 
         drive.setPoseEstimate(startPose);
 
@@ -25,10 +29,30 @@ public class BlueFarBackdrop extends LinearOpMode {
                 .splineTo(new Vector2d(48, 36), Math.toRadians(0))
                 .build();
 
+        Trajectory traj2 = drive.trajectoryBuilder(traj1.end(), true)
+                .splineTo(new Vector2d(40, 36), Math.toRadians(0))
+                .splineTo(new Vector2d(60, 60), Math.toRadians(0))
+                .build();
+
+        // -36, 28 center
+        // -26, 28 left
+        // -42, 28 right
+
         waitForStart();
 
         if(isStopRequested()) return;
 
-        drive.followTrajectory(traj1);
+        drive.followTrajectory(traj1); // navigate to backboard
+
+        bot.arm.moveArm(Arm.MAX_ARM_POSITION); // move arm up
+        bot.arm.moveSlide(Arm.MAX_SLIDE_POSITION - 300); // move slide up
+        bot.claw.openClaw(); // release the pixels
+
+        bot.claw.closeBothClaw(); // close claw so it doesn't get caught on wires
+        bot.arm.moveArm(Arm.MIN_ARM_POSITION); // retract arm
+        bot.arm.moveSlide(Arm.MIN_SLIDE_POSITION); // retract slide
+
+        drive.followTrajectory(traj2);
+
     }
 }
