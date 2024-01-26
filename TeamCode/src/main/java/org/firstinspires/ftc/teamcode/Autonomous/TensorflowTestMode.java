38959aafa88cbdab9de97fd64278bf3a0f1bf2a1
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-01-26 17:16:32 -0500

    make configurable from the dash

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TensorflowTestMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Autonomous/TensorflowTestMode.java
@@ -2,17 +2,21 @@ package org.firstinspires.ftc.teamcode.Autonomous;
 
 import static android.os.SystemClock.sleep;
 
+import com.acmerobotics.dashboard.config.Config;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
+@Config
 @Autonomous(name="TensorflowTestMode")
 public class TensorflowTestMode extends OpMode {
 
     private TensorFlowForAutonomousBlueRed tf;
 
+    public static String color = "blue";
+
     @Override
     public void init() {
-        tf = new TensorFlowForAutonomousBlueRed(hardwareMap, telemetry, "blue");
+        tf = new TensorFlowForAutonomousBlueRed(hardwareMap, telemetry, color);
         tf.initTfod();
     }
 
