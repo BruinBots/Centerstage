f38058afbc11af90fec1f493eb97d9592165435b
Christopher Mattar <69043333+3DCoded@users.noreply.github.com>
2024-01-28 15:13:51 -0500

    increase angular velocity and angular acceleration for efficiency

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -66,8 +66,8 @@ public class DriveConstants {
      */
     public static double MAX_VEL = 30;
     public static double MAX_ACCEL = 30;
-    public static double MAX_ANG_VEL = Math.toRadians(195);
-    public static double MAX_ANG_ACCEL = Math.toRadians(60);
+    public static double MAX_ANG_VEL = Math.toRadians(270); // Math.toRadians(195)
+    public static double MAX_ANG_ACCEL = Math.toRadians(90); // Math.toRadians(60)
 
     /*
      * Adjust the orientations here to match your robot. See the FTC SDK documentation for details.
